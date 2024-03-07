1af88cd2d55c3450885c937711892b6262534075
Baizid MD Ashadzzaman <86706204+Baizidmdashadzzaman@users.noreply.github.com>
2024-03-07 23:01:09 +0600

    Update TensorflowObjectDetection.jsx

--- TensorflowObjectDetection.jsx
+++ TensorflowObjectDetection.jsx
@@ -4,8 +4,7 @@ import React, { useRef, useState, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as cocossd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
-import { drawRect } from "./utilities";
-import AutoTypingLib from "../../lib/AutoTypingLib";
+import { drawRect } from "./Drawbox";
 
 function TensorflowObjectDetection() {
 
